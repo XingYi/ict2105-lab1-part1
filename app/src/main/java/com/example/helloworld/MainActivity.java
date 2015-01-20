1ea731b6aae48fb3ba357adfb9f39a4cbf08dbbf
Xing Yi <lim_xing_yi@yahoo.com>
2015-01-21 00:48:23 +0800

    Added Log.d for logging purpose.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.util.Log;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +12,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            Log.d("MyActivity", "onCreate() Restoring previous state");
+            /* restore state */
+        } else {
+            Log.d("MyActivity", "onCreate() No saved state available");
+            /* initialize app */
+        }
     }
 
 
